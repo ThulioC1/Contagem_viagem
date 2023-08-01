69ae8ebf6064968bced60e55b832a653ade9e27f
ThulioC1 <106820335+ThulioC1@users.noreply.github.com>
2023-08-01 11:34:18 -0300

    Add files via upload
    Contagem Viagem

---
+++ src/js/script.js
@@ -0,0 +1,21 @@
+function updateCountdown() {
+    const targetDate = new Date('2023-09-08T00:00:00');
+    const now = new Date().getTime();
+    const timeLeft = targetDate - now;
+
+    const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+    document.getElementById('days').innerText = formatTime(days);
+    document.getElementById('hours').innerText = formatTime(hours);
+    document.getElementById('minutes').innerText = formatTime(minutes);
+    document.getElementById('seconds').innerText = formatTime(seconds);
+}
+
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+}
+
+setInterval(updateCountdown, 1000);
